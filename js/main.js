de4dd64f9aa45434a56e57c8ce8d18c3857b1129
Jessica Jodoin <jodo0083@algonquinlive.com>
2020-04-30 11:09:21 -0400

    Add JavaScript for bubble

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var $body = $('body');
+
+$('html').on('keydown', function (e) {
+
+  var $bubble;
+
+  if (e.key == ' ') {
+    $bubble = $('<div>');
+    $bubble.addClass('bubble');
+    $bubble.css({
+      'top': Math.random() * (document.documentElement.clientHeight - 100),
+      'left': Math.random() * (document.documentElement.clientWidth - 100)
+    });
+    $bubble.append($bubble);
+  }
+});
